736854d231cbd3b12fc6e9cd57080bc91cb1b7ff
abaurens <abaurens@student.42.fr>
2024-05-07 13:35:55 +0200

    Pulled master

---
+++ premake5.lua
@@ -0,0 +1,20 @@
+-- glm
+
+project "glm"
+  kind "None"
+  language "C++"
+  cppdialect "C++17"
+  systemversion "latest"
+
+  targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+  objdir ("%{wks.location}/build/" .. outputdir .. "%{prj.name}")
+
+  IncludeDir["glm"] = "%{wks.location}/libs/glm"
+
+  includedirs {}
+
+  files {
+    "premake5.lua",
+    "glm/**.hpp",
+    "glm/**.h",
+  }
